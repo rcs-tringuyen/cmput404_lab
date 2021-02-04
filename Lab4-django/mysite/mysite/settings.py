0b4cacea4819476e2761d190eb474c3e26db65ae
rcs-tringuyen <tlnguyen@ualberta.ca>
2021-02-03 23:14:27 -0700

    Finish lab 4

--- Lab4-django/mysite/mysite/settings.py
+++ Lab4-django/mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h3ob*mp5!szzcecbv*fuwu-!^@&o)afie9#2g30ptos-@5@q9s'
+SECRET_KEY = '@r)3arql&$23co23z88gp4j!3qbkdb2@m$0&1m@8hk8wxf=ui('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
